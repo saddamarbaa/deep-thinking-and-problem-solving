f1d82855831faad075c457b316f90c7547450b20
saddamarbaa <saddamarbaas@gmail.com>
2020-12-03 00:59:22 +0700

    Challenge 1: Add Two Numbers

---
+++ addNumbers.js
@@ -0,0 +1,14 @@
+// Challenge 1: Add Two Numbers
+
+// function to add two numbers and return the result
+
+const addNumbers = function(num1, num2) {
+    return num1 + num2;
+};
+
+// Call function many time and pass argument for testing
+
+console.log(addNumbers(2, 3)); // output: 5
+console.log(addNumbers(12, 54)); // output: 66
+console.log(addNumbers(7, 13)); // output: 20
+console.log(addNumbers(5, 8)); // output: 13
