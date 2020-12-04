115e008b75cd3594f1d6334c7bf7276df2f6f88c
saddamarbaa <saddamarbaas@gmail.com>
2020-12-05 02:35:17 +0700

    Challenge 10: How to Use Helper Functions

--- sortbyBiggestNumbersImprovedVersion.js
+++ sortbyBiggestNumbersImprovedVersion.js
@@ -1,28 +1,38 @@
-// Challenge 9: Sort by Biggest Numbers
+// Challenge 10: How to Use Helper Functions
+
+// Helper function to Find the Maximum value in array 
+
+const findMaxHelper = function(Numbers, startIndex) {
+    // label first value to be the maximum 
+    let maximum = Numbers[startIndex];
+    let maxLocation = startIndex;
+    // iterate over the array starting from the given index +1
+    for (let i = startIndex + 1; i < Numbers.length; i++) {
+        if (Numbers[i] > maximum) {
+            // find max item and its index
+            maximum = Numbers[i]
+            maxLocation = i;
+        }
+    }
+    // return object with max item and its index 
+    return { maxNumber: maximum, maxIndex: maxLocation };
+};
 
 //  Given a list of movies and their rating ..
 //  you need to sort the list in a descending order 
 
-const sortBestRatingsFirst = function(Numbers) {
-    // sortBestRatingsFirst([5, 8, 2, 9, 3, 10]) ---> [ 10, 9, 8, 5, 3, 2 ]
-    // outer loop
-    // findMax
+const sortBestRatingsWithHelper = function(Numbers) {
+    // run as many times as there are items
     for (let j = 0; j < Numbers.length - 1; j++) {
-        let maximum = Numbers[j];
-        let maxLocation = j;
-        // inner loop
-        for (let i = j + 1; i < Numbers.length; i++) {
-            // to sort in ascending order just change 
-            // ( Numbers[i] > maximum) to (Numbers[i] < maximum)
-            if (Numbers[i] > maximum) {
-                // know max AND it,s index(location)
-                maximum = Numbers[i];
-                maxLocation = i;
-            }
-        }
-        // Swap the values 
+        // find max number and max location starting from j
+        let max = findMaxHelper(Numbers, j);
+        let maximum = max['maxNumber'];
+        let maxLocation = max['maxIndex'];
+
+        // Swap the first and max item in an array
         Numbers[maxLocation] = Numbers[j];
         Numbers[j] = maximum;
+
     }
     // return the sorted array
     return Numbers;
@@ -30,6 +40,7 @@ const sortBestRatingsFirst = function(Numbers) {
 
 // Call function many time and pass argument for testing
 
-console.log(sortBestRatingsFirst([5, 8, 2, 9, 3, 10])); // output : [ 10, 9, 8, 5, 3, 2 ]
-console.log(sortBestRatingsFirst([20, 30, 40, 50])); // output : [ 50, 40, 30, 20 ]
-console.log(sortBestRatingsFirst([5, 9, 8, 1, 12, 3, 10])); // output :[12, 10, 9, 8, 5, 3, 1]
+console.log(sortBestRatingsWithHelper([2, 3, 11])); // output : [ 11, 3, 2 ]
+console.log(sortBestRatingsWithHelper([5, 8, 2, 9, 3, 10])); // output : [ 10, 9, 8, 5, 3, 2 ]
+console.log(sortBestRatingsWithHelper([20, 30, 40, 50])); // output : [ 50, 40, 30, 20 ]
+console.log(sortBestRatingsWithHelper([5, 9, 8, 1, 12, 3, 10])); // output :[12, 10, 9, 8, 5, 3, 1]
