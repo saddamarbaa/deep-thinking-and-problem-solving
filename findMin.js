994bd5cf473afa09ae21c9486d1f442a7fd1cc35
saddamarbaa <saddamarbaas@gmail.com>
2020-12-04 17:28:25 +0700

    Challenge 7: Finding the Minimum

--- findMin.js
+++ findMin.js
@@ -1,23 +1,34 @@
-// Challenge 7: Finding the Maximum 
+// Challenge 7: Finding the Minimum
 
-// function to Find the Maximum  value in array (take array as parameter)
+// function to Find the minimum  value in array (take array as parameter)
 // let assume is array of numbers
 
-const findMax = function(array) {
-    // label first value to be the maximum   
-    let maximum = array[0];
+const findMin = function(array) {
+    // label first value to be the minimum  
+    let minimum = array[0];
     // iterate over the array starting from the second index
     for (let index = 1; index < array.length; index++) {
-        // if found new maximum  value then update maximum  variable
-        if (array[index] < maximum) {
-            maximum = array[index]
+        // if found new minimum value then update minimum variable
+        if (array[index] < minimum) {
+            minimum = array[index]
         }
     }
-    // return the maximum  value
-    return maximum
+    // return the minimum value
+    return minimum
 };
 
-console.log(findMax([1, 2, 3, 4])); // output : 1
-console.log(findMax([16, 22, 3, 4, 2])); // output : 2
-console.log(findMax([7, 15, 5, 6, 8, 15, 30, 500, 0])); // output : 0
-console.log(findMax([16, 12, 4, 44, 23, 4])); // output : 4
+console.log(findMin([1, 2, 3, 4])); // output : 1
+console.log(findMin([16, 22, 3, 4, 2])); // output : 2
+console.log(findMin([7, 15, 5, 6, 8, 15, 30, 500, 0])); // output : 0
+console.log(findMin([16, 12, 4, 44, 23, 4])); // output : 4
+
+// Math.min()
+// Getting the Minimum element of an array
+// Array.reduce() can be used to find the Minimum element in a numeric array, by comparing each value
+
+let arr = [11, 200, 3000, 8, 54];
+let min = arr.reduce(function(a, b) {
+    return Math.min(a, b);
+});
+
+console.log(min); // output : 8
