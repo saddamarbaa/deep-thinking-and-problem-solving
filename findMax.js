4d12276eb579e18caf0057dba68cf0c7a72c2050
saddamarbaa <saddamarbaas@gmail.com>
2020-12-04 16:58:38 +0700

    Challenge 8: Finding the Maximum

---
+++ findMax.js
@@ -0,0 +1,23 @@
+// Challenge 8: Finding the Maximum
+
+// function to Find the Maximum value in array (take array as parameter)
+// let assume is array of numbers
+
+const findMax = function(array) {
+    // label first value to be the maximum 
+    let maximum = array[0];
+    // iterate over the array starting from the second index
+    for (let index = 1; index < array.length; index++) {
+        // if found new maximum  value then update maximum variable
+        if (array[index] > maximum) {
+            maximum = array[index]
+        }
+    }
+    // return the maximum  value
+    return maximum
+};
+
+console.log(findMax([1, 2, 3, 4])); // output : 4
+console.log(findMax([16, 22, 3, 4, 2])); // output : 22
+console.log(findMax([7, 15, 5, 6, 8, 15, 30, 500, 0])); // output : 500
+console.log(findMax([16, 12, 4, 44, 23, 4])); // output : 44
--- findMin.js
+++ findMin.js
@@ -1,23 +1,23 @@
-// Challenge 7: Finding the Minimum
+// Challenge 7: Finding the Maximum 
 
-// function to Find the Minimum value in array (take array as parameter)
+// function to Find the Maximum  value in array (take array as parameter)
 // let assume is array of numbers
 
-const findMin = function(array) {
-    // label first value to be the minimum  
-    let minimum = array[0];
+const findMax = function(array) {
+    // label first value to be the maximum   
+    let maximum = array[0];
     // iterate over the array starting from the second index
     for (let index = 1; index < array.length; index++) {
-        // if found new minimum value then update minimum variable
-        if (array[index] < minimum) {
-            minimum = array[index]
+        // if found new maximum  value then update maximum  variable
+        if (array[index] < maximum) {
+            maximum = array[index]
         }
     }
-    // return the minimum value
-    return minimum
+    // return the maximum  value
+    return maximum
 };
 
-console.log(findMin([1, 2, 3, 4])); // output : 1
-console.log(findMin([16, 22, 3, 4, 2])); // output : 2
-console.log(findMin([7, 15, 5, 6, 8, 15, 30, 500, 0])); // output : 0
-console.log(findMin([16, 12, 4, 44, 23, 4])); // output : 4
+console.log(findMax([1, 2, 3, 4])); // output : 1
+console.log(findMax([16, 22, 3, 4, 2])); // output : 2
+console.log(findMax([7, 15, 5, 6, 8, 15, 30, 500, 0])); // output : 0
+console.log(findMax([16, 12, 4, 44, 23, 4])); // output : 4
